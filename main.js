bb10bc662ec355d9f8b5cc4f150dd9e9abb90956
olaniyiOlawale <jao.olaniyi@gmail.com>
2022-03-15 14:25:40 +0100

    done

--- main.js
+++ main.js
@@ -1,39 +1,12 @@
-// var good = 'bad';
-// document.getElementById('time').innerHTML = good;
-//  var onclick="document.getElementById('time').innerHTML = Date()"
 
-// var a = 6;
-// var b = 7;
-// var c = a + b;
-//  document.getElementById("learn").innerHTML = c;
 var sec = 60;
 var min = 60;
 var day = sec * min * 24;
 var week = 7 * day
- document.getElementById("day").innerHTML = week;
+ document.getElementById("day").innerHTML = week + "s";
  console.log(week)
  alert(week);
 
 
 
 
-// var carName = "";
-// document.getElementById("demo").innerHTML = carName;
-// var text = "command";
-// document.getElementById("learn").innerHTML = text;
-
-// var userAge = prompt("Enter your age: ");
-// document.getElementById("userAge").innerHTML = userAge;
-// age.innerHTML = userAge;
-// var userPhone = prompt("Enter your phone:");
-// // document.getElementById("phone").innerHTML = phone;
-// phone.innerHTML = userPhone;
-// var question = prompt("Enter your address:");
-// document.getElementById("question").innerHTML = address;
-// // address.innerHTML = question;
-// var color = prompt("Enter your color:");
-// document.getElementById("color").innerHTML = color;
-// color.innerHTML = color;
-
-//let name = "wale"
-//alert (" welcome to ATC" + userAge)
